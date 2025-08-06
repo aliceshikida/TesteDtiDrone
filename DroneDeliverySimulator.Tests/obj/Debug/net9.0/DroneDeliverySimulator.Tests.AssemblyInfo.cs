4b43034ff5768281930f0023d59713b328fe5953
Alice Shikida <aliceshikida12@gmail.com>
2025-08-06 15:24:57 -0300

    commit projeto

--- DroneDeliverySimulator.Tests/obj/Debug/net9.0/DroneDeliverySimulator.Tests.AssemblyInfo.cs
+++ DroneDeliverySimulator.Tests/obj/Debug/net9.0/DroneDeliverySimulator.Tests.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DroneDeliverySimulator")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DroneDeliverySimulator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eff169ed4e999e3b07071d29dcf5d4fb440e723")]
-[assembly: System.Reflection.AssemblyProductAttribute("DroneDeliverySimulator")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DroneDeliverySimulator")]
+[assembly: System.Reflection.AssemblyProductAttribute("DroneDeliverySimulator.Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DroneDeliverySimulator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
