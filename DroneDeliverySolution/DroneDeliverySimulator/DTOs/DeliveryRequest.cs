12b0168aa6bab4b63afe73578557899c59d8126e
Alice Shikida <161458492+aliceshikida@users.noreply.github.com>
2025-08-05 23:58:51 -0300

    Delete DroneDeliverySolution directory

--- DroneDeliverySolution/DroneDeliverySimulator/DTOs/DeliveryRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace DroneDeliverySimulator.DTOs;
-
-public class DeliveryRequest
-{
-    public string Destino { get; set; }
-}
--- DroneDeliverySolution/DroneDeliverySimulator/Models/Delivery.cs
+++
@@ -1,8 +0,0 @@
-namespace DroneDeliverySimulator.Models;
-
-public class Delivery
-{
-    public int Id { get; set; }
-    public string Destino { get; set; }
-    public string Status { get; set; } = "Pendente";
-}
